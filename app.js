83d08a2fc0494273d99efb045d02bd131d4d5d9c
Jeferson-Djeife-Nardelli <jefersonnardelli17@gmail.com>
2025-12-22 21:12:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const balanceEl = document.getElementById("balance");
+const incomeEl = document.getElementById("income");
+const expensesEl = document.getElementById("expenses");
 
+// Temporary numbers (mock data)
+const income = 2500;
+const expenses = 1200;
+const balance = income - expenses;
+
+incomeEl.textContent = `$${income.toFixed(2)}`;
+expensesEl.textContent = `$${expenses.toFixed(2)}`;
+balanceEl.textContent = `$${balance.toFixed(2)}`;
